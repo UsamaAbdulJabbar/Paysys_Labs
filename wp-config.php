5e63b7f57bbaab1e4f52e0d0a8b596377fab3986
Usama Abdul Jabbar <94742276+UsamaAbdulJabbar@users.noreply.github.com>
2025-05-05 19:43:12 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,8 @@ foreach ($_SERVER as $key => $value) {
         continue;
     }
     
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    //$connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbhost = "localdb_new";
     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
